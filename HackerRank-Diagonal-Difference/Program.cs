244cefdad06d02ad24c7105f09ea14ac56bce86d
jaboud <jaboud94@gmail.com>
2019-09-12 15:40:50 +1000

    Created sum value for each left and right diagonals.

--- HackerRank-Diagonal-Difference/Program.cs
+++ HackerRank-Diagonal-Difference/Program.cs
@@ -8,7 +8,9 @@ namespace HackerRank_Diagonal_Difference
         //Static method for the arrays using the integer type and implenting the list class.
         public static int DiagonalDifference(List<List<int>> arr)
         {
-
+            //Sums for each left and right diagonals and set the default value at 0.
+            int sumlr = 0;
+            int sumrl = 0;
         }
         static void Main(string[] args)
         {
