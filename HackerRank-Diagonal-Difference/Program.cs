968f0fdc96131622c013ed65d30760dba23c56fd
jaboud <jaboud94@gmail.com>
2019-09-12 15:52:17 +1000

    For loop to get the length of arrays. Modified the static int method and removed the list class to allow the .Length type as an variable so we could use it in the for loop.

--- HackerRank-Diagonal-Difference/Program.cs
+++ HackerRank-Diagonal-Difference/Program.cs
@@ -5,12 +5,22 @@ namespace HackerRank_Diagonal_Difference
 {
     class Program
     {
-        //Static method for the arrays using the integer type and implenting the list class.
-        public static int DiagonalDifference(List<List<int>> arr)
+        //Static method for the arrays using the integer type.
+        public static int DiagonalDifference(int[][] arr)
         {
             //Sums for each left and right diagonals and set the default value at 0.
             int sumlr = 0;
             int sumrl = 0;
+            int length = arr[0].Length;
+            //For loop to get the length of the rows of the array,
+            for (int i = 0; i > length; i++)
+            {
+                sumlr += arr[i][0 + i];
+                sumrl += arr[i][length - i - 1];
+            }
+
+            return Math.Abs(sumlr - sumrl);
+
         }
         static void Main(string[] args)
         {
