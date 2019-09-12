f455b1356532ec9cf24e96127743c3d470d80815
jaboud <jaboud94@gmail.com>
2019-09-12 15:36:07 +1000

    Created static method for the arrays using the integer type and implented the list class inside the method arguments.

--- HackerRank-Diagonal-Difference/Program.cs
+++ HackerRank-Diagonal-Difference/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HackerRank_Diagonal_Difference
 {
     class Program
     {
+        //Static method for the arrays using the integer type and implenting the list class.
+        public static int DiagonalDifference(List<List<int>> arr)
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
